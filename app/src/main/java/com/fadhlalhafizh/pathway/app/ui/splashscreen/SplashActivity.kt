cf074a0997b1d0da28bdd757b6adf6cf6f7fa06b
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-12-12 18:34:08 +0700

    Add mipmap and implementation logic

--- app/src/main/java/com/fadhlalhafizh/pathway/app/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/fadhlalhafizh/pathway/app/ui/splashscreen/SplashActivity.kt
@@ -1,12 +1,34 @@
 package com.fadhlalhafizh.pathway.app.ui.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import com.fadhlalhafizh.pathway.R
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.fadhlalhafizh.pathway.app.ui.main.MainActivity
+import com.fadhlalhafizh.pathway.app.ui.welcome.WelcomeActivity
+import com.fadhlalhafizh.pathway.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 3000L)
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, WelcomeActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
     }
 }
