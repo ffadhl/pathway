9b5d04d58e7b42b2a4a273be5a9613b13bef6705
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-12-21 19:59:50 +0700

    fix UniversityFragment

--- app/src/main/java/com/fadhlalhafizh/pathway/app/adapter/university/UnivUniversityAdapter.kt
+++ app/src/main/java/com/fadhlalhafizh/pathway/app/adapter/university/UnivUniversityAdapter.kt
@@ -4,37 +4,43 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.fadhlalhafizh.pathway.R
 import com.fadhlalhafizh.pathway.app.ui.detailuniversity.DetailUniversityActivity
+import com.fadhlalhafizh.pathway.data.model.University
 
-class UnivUniversityAdapter: RecyclerView.Adapter<UnivUniversityAdapter.MyViewHolder>() {
-    class MyViewHolder(item : View): RecyclerView.ViewHolder(item) {
-
+class UnivUniversityAdapter(private val listUniversity: ArrayList<University>) :
+    RecyclerView.Adapter<UnivUniversityAdapter.MyViewHolder>() {
+    class MyViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.iv_university)
+        val imgPhotoBackground: ImageView = itemView.findViewById(R.id.iv_background_university)
+        val universityName: TextView = itemView.findViewById(R.id.tv_universityNames)
+        val universityAddress: TextView = itemView.findViewById(R.id.tv_universityAddress)
+        val universityWorldRank: TextView = itemView.findViewById(R.id.tv_worldRank2)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_university, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_university, parent, false)
         return MyViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return 10
-    }
+    override fun getItemCount(): Int = listUniversity.size
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val universityName = "Sample University"
-        val universityAddress = "Sample Address"
-        val worldRank1 = "World Rank"
-        val worldRank2 = "x"
+        val (name, address, description, worldRank, universityPhoto, backgroundPhoto) = listUniversity[position]
+        holder.imgPhoto.setImageResource(universityPhoto)
+        holder.imgPhotoBackground.setImageResource(backgroundPhoto)
+        holder.universityName.text = name
+        holder.universityAddress.text = address
+        holder.universityWorldRank.text = worldRank
 
         holder.itemView.setOnClickListener {
-            val intent = Intent(it.context, DetailUniversityActivity::class.java)
-            intent.putExtra(DetailUniversityActivity.UNIVERSITY, universityName)
-            intent.putExtra(DetailUniversityActivity.ADDRESS, universityAddress)
-            intent.putExtra(DetailUniversityActivity.WORLD_RANK1, worldRank1)
-            intent.putExtra(DetailUniversityActivity.WORLD_RANK2, worldRank2)
-            it.context.startActivity(intent)
+            val intentDetail = Intent(holder.itemView.context, DetailUniversityActivity::class.java)
+            intentDetail.putExtra("key_University", listUniversity[holder.adapterPosition])
+            holder.itemView.context.startActivity(intentDetail)
         }
     }
 }
--- app/src/main/java/com/fadhlalhafizh/pathway/app/ui/main/ui/university/UniversityFragment.kt
+++ app/src/main/java/com/fadhlalhafizh/pathway/app/ui/main/ui/university/UniversityFragment.kt
@@ -1,5 +1,6 @@
 package com.fadhlalhafizh.pathway.app.ui.main.ui.university
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,13 +11,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fadhlalhafizh.pathway.R
 import com.fadhlalhafizh.pathway.app.adapter.university.UnivUniversityAdapter
+import com.fadhlalhafizh.pathway.data.model.University
 import com.fadhlalhafizh.pathway.databinding.FragmentUniversityBinding
 
 class UniversityFragment : Fragment() {
 
     private var _binding: FragmentUniversityBinding? = null
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: UnivUniversityAdapter
+    private lateinit var rvUniversity: RecyclerView
+    private val listUniversity = ArrayList<University>()
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -30,14 +32,59 @@ class UniversityFragment : Fragment() {
         _binding = FragmentUniversityBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        recyclerView = root.findViewById(R.id.rv_fragmentUniversity)
-        adapter = UnivUniversityAdapter()
-
-        recyclerView.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = adapter
+        rvUniversity = binding.rvFragmentUniversity
+        rvUniversity.setHasFixedSize(true)
+        listUniversity.addAll(getListUniversity())
+        showRecyclerList()
 
         return root
+    }
+
+    @SuppressLint("Recycle")
+    private fun getListUniversity(): ArrayList<University> {
+        val dataUniversity = resources.getStringArray(R.array.data_university)
+        val dataAddress = resources.getStringArray(R.array.data_address)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataWorldRank = resources.getStringArray(R.array.data_worldRank)
+        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+        val dataPhotoBackground = resources.obtainTypedArray(R.array.data_photoBackground)
+
+        val minSize = minOf(
+            dataUniversity.size,
+            dataAddress.size,
+            dataDescription.size,
+            dataWorldRank.size,
+            dataPhoto.length(),
+            dataPhotoBackground.length()
+        )
+
+        val listUniversity = ArrayList<University>()
+
+        for (i in 0 until minSize) {
+            val univ = University(
+                dataUniversity[i],
+                dataAddress[i],
+                dataDescription[i],
+                if (i < dataWorldRank.size) dataWorldRank[i] else "",
+                if (i < dataPhoto.length()) dataPhoto.getResourceId(i, -1) else -1,
+                if (i < dataPhotoBackground.length()) dataPhotoBackground.getResourceId(
+                    i,
+                    -1
+                ) else -1
+            )
+            listUniversity.add(univ)
+        }
+
+        return listUniversity
+    }
+
+
+    private fun showRecyclerList() {
+        val layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        rvUniversity.layoutManager = layoutManager
+        val listUniversityAdapter = UnivUniversityAdapter(listUniversity)
+        rvUniversity.adapter = listUniversityAdapter
     }
 
     override fun onDestroyView() {
