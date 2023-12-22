938776a256c6072a9e49e548f493f3dc74d958f2
Fadhl Al-Hafizh <fadhlhafizh18@students.unnes.ac.id>
2023-12-22 09:19:22 +0700

    Fix Layout

--- app/src/main/java/com/fadhlalhafizh/pathway/data/remote/response/ResultMajorResponse.kt
+++ app/src/main/java/com/fadhlalhafizh/pathway/data/remote/response/ResultMajorResponse.kt
@@ -4,6 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 data class ResultMajorResponse(
 
+	@field:SerializedName("Recommended alternatif")
+	val recommendedAlternatif: List<String>,
+
+	@field:SerializedName("Rekomendasi Karir")
+	val rekomendasiKarir: List<String>,
+
+	@field:SerializedName("similarities")
+	val similarities: List<Any>,
+
 	@field:SerializedName("Prediksi Jurusan")
 	val prediksiJurusan: String,
 
